b77a0f8e69bbc7a15855638b75044716e60003c7
mehmetresitcagan <mresitcagan@gmail.com>
2021-08-19 14:24:59 +0300

    add responsive forms

--- blog/settings.py
+++ blog/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "article",
-    "user",
+    'article',
+    'user',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
