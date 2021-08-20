3d5d06cd600cff338c510b83ec9c6de1dd948644
mehmetresitcagan <mresitcagan@gmail.com>
2021-08-20 15:53:28 +0300

    some fixes

--- article/views.py
+++ article/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import AddArticleForm
+from django.contrib import messages
+from .models import Article
 
 
 # Create your views here.
+
 
 def index(request):
     return render(request, "index.html")
@@ -13,9 +16,20 @@ def about(request):
 
 
 def dashboard(request):
-    return render(request, "dashboard.html")
+    articles = Article.objects.filter()
+    context = {
+        "articles" : articles
+    }
+    return render(request, "dashboard.html", context)
 
 
 def addArticle(request):
-    form = AddArticleForm()
+    form = AddArticleForm(request.POST or None)
+    if form.is_valid():
+        article = form.save(commit=False)
+        article.author = request.user
+        article.save()
+
+        messages.success(request, "Article has been created successfully.")
+        return redirect("index")
     return render(request, "addArticle.html", {"form": form})
