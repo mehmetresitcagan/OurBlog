711cba592903ac5fdd10634b1b7697f075194ffd
mehmetresitcagan <mresitcagan@gmail.com>
2021-08-22 11:50:36 +0300

    add ckeditor app

--- article/models.py
+++ article/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
 
 class Article(models.Model):
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    content = models.TextField()
+    content = RichTextField()
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- blog/settings.py
+++ blog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'article',
     'user',
     'crispy_forms',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+CKEDITOR_CONFIGS = {
+    "default": {
+        "removePlugins": "stylesheetparser",
+        "allowedContent": True,
+        "width": "100%",
+    },
+}
